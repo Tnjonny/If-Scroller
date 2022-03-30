6ee5f87910959f2f16c3bbc58af4980bd323b0de
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2022-03-30 20:39:28 +0100

    commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-let photosArray = [];
-
 const count = 10;
 const apiKey = '8QI3AvNlYA4iEYTSoCQAO4YhuBbwJDPvm3kz2lfwK7Y';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+let photosArray = [];
 
 function setAttribute(element, attributes) {
   for (const key in attributes) {
@@ -16,19 +16,13 @@ function setAttribute(element, attributes) {
 function displayPhotos() {
   photosArray.forEach((photo) => {
     const item = document.createElement('a');
-    // item.setAttribute('href', photo.links.html);
-    // item.setAttribute('target', '_blank');
 
     const img = document.createElement('img');
 
     setAttribute(item, {
       href: photo.links.html,
-      target: '_blank',
+      target: 'blank',
     });
-
-    // img.setAttribute('src', photo.urls.regular);
-    // img.setAttribute('alt', photo.alt_description);
-    // img.setAttribute('title', photo.alt_description);
 
     setAttribute(img, {
       src: photo.urls.regular,
@@ -45,8 +39,17 @@ async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
+    displayPhotos();
   } catch (error) {}
 }
 
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    getPhotos();
+  }
+});
+
 getPhotos();
-displayPhotos();
